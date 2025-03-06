c5df2337ea0baef2e47c892e9384c112c901989a
Hammad ul Rehman <hammad2006rehman@gmail.com>
2025-03-06 22:24:30 +0500

    Creating new app named as search for our product api

--- drf/backend/cfehome/cfehome/urls.py
+++ drf/backend/cfehome/cfehome/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('api.urls')),   # localhost:8000/api/
-    path('api/products/',include('products.urls')),
-    path('api/v2/',include('cfehome.routers'))
+    path('api/', include('api.urls')),   # localhost:8000/api/
+    path('api/search/', include('search.urls')),
+    path('api/products/', include('products.urls')),
+    path('api/v2/', include('cfehome.routers'))
 ]
